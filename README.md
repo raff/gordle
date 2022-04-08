7eb9d3135aa30075e8f61b2e4f0a7e2cca1c8d34
Raffaele Sena <raff367@gmail.com>
2022-04-07 22:53:50 -0700

    Added -list mode to just list the worlde results instead of adding to skip list

--- README.md
+++ README.md
@@ -7,12 +7,18 @@ gordle is a helper for wordle
 
 ### Where:
 
-    -words string
-        external file with words (i.e. "/usr/share/dict/words" on *nix like environments)
-    -skip string
-        skip words containing any of these letters
+    -all
+        show all words (answer and allowed) - false: only show valid answer words
+    -list
+        list mode: the first word is the initial one, the remaining show the results
     -contain string
         accept only words that contain all these letters
+    -skip string
+        skip words containing any of these letters
+    -sort
+        sort by frequency (default true)
+    -words string
+        external file with words
 
 This commands prints a list of 5 letter words matching the specified parameters.
 
@@ -26,7 +32,3 @@ It is also possible to specify letters that should be in the word but are in the
 For example in the following pattern, the letter `C` is in the right place (first letter of the word) but the letters `ra` are in the wrong place:
 
 	C--ra
-
-
-
-    
--- gordle.go
+++ gordle.go
@@ -18,9 +18,7 @@ var (
 	//go:embed gordle.list
 	worddata string
 
-	words    []string
-	skips    string
-	contains string
+	words []string
 )
 
 func ContainsAll(s, chars string) bool {
@@ -41,6 +39,10 @@ func IsUpper(c rune) bool {
 	return c >= 'A' && c <= 'Z'
 }
 
+func IsLetter(c rune) bool {
+	return IsLower(c) || IsUpper(c)
+}
+
 func ToLower(c rune) rune {
 	return c - 'A' + 'a'
 }
@@ -50,11 +52,14 @@ func ToUpper(c rune) rune {
 }
 
 func main() {
+	var skips, contains string
+
 	wordfile := flag.String("words", "", "external file with words")
 	flag.StringVar(&skips, "skip", "", "skip words containing any of these letters")
 	flag.StringVar(&contains, "contain", "", "accept only words that contain all these letters")
 	fsort := flag.Bool("sort", true, "sort by frequency")
 	all := flag.Bool("all", false, "show all words (answer and allowed) - false: only show valid answer words")
+	asList := flag.Bool("list", false, "list mode: the first word is the initial one, the remaining show the results")
 	flag.Parse()
 
 	skips = strings.ToUpper(strings.TrimSpace(skips))
@@ -99,23 +104,53 @@ func main() {
 
 	sort.Strings(words)
 
-	matches := ""
+	if *asList {
+		processList(flag.Args(), *fsort)
+	} else if flag.NArg() > 0 {
+		processWord(flag.Arg(0), skips, contains, *fsort)
+	} else {
+		printList(words, *fsort)
+	}
+}
 
-	if flag.NArg() > 0 {
-		matches = strings.TrimSpace(flag.Arg(0))
-
-		if len(matches) != 5 {
-			log.Fatal("5 letter word")
-		}
+func processWord(word, skips, contains string, sort bool) {
+	word = strings.TrimSpace(word)
+	if len(word) != 5 {
+		log.Fatalf("expected 5 letter word: %q", word)
 	}
 
-	list := match(matches)
+	list := match(word, skips, contains)
 	if len(list) == 0 {
 		fmt.Println("No matches")
 		return
 	}
 
-	if !*fsort {
+	printList(list, sort)
+}
+
+func processList(words []string, sort bool) {
+	var word, skips string
+
+	for _, w := range words {
+		w = strings.TrimSpace(w)
+		if len(w) != 5 {
+			log.Fatalf("expected 5 letter word: %q", w)
+		}
+
+		for p, c := range w {
+			if !IsLetter(c) {
+				skips += word[p : p+1]
+			}
+		}
+
+		word = w
+	}
+
+	processWord(word, skips, "", sort)
+}
+
+func printList(list []string, sort bool) {
+	if !sort {
 		fmt.Println(list)
 		return
 	}
@@ -140,7 +175,7 @@ func main() {
 	}
 }
 
-func match(m string) (res []string) {
+func match(m, skips, contains string) (res []string) {
 	misplaced := ""
 
 	for _, c := range m {
